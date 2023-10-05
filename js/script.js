0461a567560fc72d02894daabcb0231dad05ed97
shahruk-maharuj <shahrukmaharuj10@gmail.com>
2023-10-05 09:09:50 +0600

    add js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
-// code here
+'use strict';
+
+// DOM element references
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+// Open modal function
+const openModal = () => {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+// Close modal function
+const closeModal = () => {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+// Open modal on button click
+for (let i = 0; i < btnsOpenModal.length; i++) {
+    btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+// Event listeners to close modal
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
